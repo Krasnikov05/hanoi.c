7a9dec97d31f0377aeb5a4bcace83dcdd0b80f83
Krasnikov Kirill <krasnikov.kirill@outlook.com>
2024-03-31 20:59:04 +0300

    animate.sh: no clear

--- animate.sh
+++ animate.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 bash ./build.sh
+function exit_ok() {
+  exit 0
+}
+trap exit_ok INT
 while :
 do
-  clear
   {
     while :
     do
+      echo -e '\e[K' > /dev/stderr
       echo
       sleep 0.1
     done
